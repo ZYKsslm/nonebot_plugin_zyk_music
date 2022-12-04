2372d340a776bb01ab5b8b093d258a2998820bd2
ZYKsslm <3119964735@qq.com>
2022-12-04 12:59:52 +0800

    :pushpin: version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nonebot_plugin_zyk_music",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="ZSSLM",
     author_email="3119964735@qq.com",
