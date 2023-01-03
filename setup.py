5328ea93317235569cc5cfe6184abf9060a62afa
ZSSLM <3119964735@qq.com>
2023-01-03 11:11:00 +0800

    :pushpin: version 0.1.4

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="nonebot_plugin_zyk_music",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     author="ZSSLM",
     author_email="3119964735@qq.com",
-    long_description="This is a simple plugin to pick up music",
+    long_description="This is a simple plugin for nonebot2 to pick up music",
     url="https://github.com/ZYKsslm/nonebot_plugin_zyk_music",
     license="MIT License",
     install_requires=["fake_useragent", "httpx", "aiohttp", "colorama", "nonebot2", "nonebot_adapter_onebot"],
